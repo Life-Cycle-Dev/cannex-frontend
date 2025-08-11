0c774ee4151565e142572842a5dad33cfb26a9fc
Thanaphorn Wutthikunprapan <thanaphornwutthikunprapan@Mac-mini-khxng-Thanaphorn.local>
2025-08-12 01:20:42 +0700

    feat: text banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,44 @@ export default function Home() {
           alt="Hero Background"
           className="absolute inset-0 z-0"
         />
-        <div className="relative z-10 flex flex-col gap-8">
-          <h1 className="text-5xl font-bold">Pharmaceutical-</h1>
+        <div className="relative z-10 flex flex-col gap-8 left-5 desktop:left-20 top-12">
+          <div>
+            <div className="h-[51px] desktop:h-20">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-crystalGreen w-fit">
+                Pharmaceutical-
+              </h1>
+            </div>
+            <div className="h-[51px] desktop:h-20">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-crystalGreen w-fit">
+                -Grade
+              </h1>
+            </div>
+            <div className="h-[51px] desktop:h-20">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-crystalGreen w-fit">
+                Formulation,
+              </h1>
+            </div>
+            <div className="h-[51px] desktop:h-20">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-white w-fit">
+                Crafted with
+              </h1>
+            </div>
+            <div className="hidden desktop:block desktop:h-20">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-white w-fit">
+                Clinical Precision.
+              </h1>
+            </div>
+            <div className="h-[51px] desktop:hidden">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-white w-fit">
+                Clinical
+              </h1>
+            </div>
+            <div className="h-[51px] desktop:hidden">
+              <h1 className="text-5xl desktop:text-7xl font-bold text-black bg-white w-fit">
+                Precision.
+              </h1>
+            </div>
+          </div>
           <p className="font-semibold">
             From plant to product — Cannex unites American genetics, Thai
             innovation, and Japanese-grade extraction to deliver premium medical
