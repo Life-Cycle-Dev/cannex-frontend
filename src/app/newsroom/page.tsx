5eb0172700efdf47c9dbc01249ec46773d6d8593
algnot <thanawat.tongla@gmail.com>
2025-08-12 04:23:30 +0700

    feat: add axios dependency and update Newsroom page with carousel component

--- src/app/newsroom/page.tsx
+++ src/app/newsroom/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import NewsroomCarousel from "@/components/NewsroomCarousel";
+import { BackendClient } from "@/lib/backend-client";
+import React from "react";
 
-export default function Page() {
+export default async function Page() {
+  const backendClient = new BackendClient();
+  const response = await backendClient.getNewsRoomsPagination({
+    "pagination[withCount]": "true",
+    "pagination[pageSize]": 3,
+    "pagination[page]": 1,
+  });
+
+  const items = response?.data ?? [];
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="flex justify-between">
+        <div className="min-w-[79px] h-[176px] border-r-2 hidden tablet:block"></div>
+        <div className="w-full font-bold text-4xl p-[32px_0px_32px_32px] tablet:text-7xl tablet:p-[64px_0px_32px_40px]">
+          Newsroom
+        </div>
+        <div className="min-w-[79px] h-[176px] border-l-2 hidden tablet:block"></div>
+      </div>
+      <div className="border-t-2" />
+      <NewsroomCarousel items={items} />
+    </div>
+  );
 }
--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -6,7 +6,7 @@ import NavBarBase from "./NavBarBase";
 import Hamburger from "./Hamburger";
 import Menu from "./Menu";
 import Image from "next/image";
-import Button from "../Button";
+import Button from "@/components/Button";
 
 type NavState = "navbar" | "navbarMenu";
 interface NavBarProps {
@@ -47,17 +47,17 @@ export default function NavBar({ state = "navbar" }: NavBarProps) {
   }, [open]);
 
   return (
-    <header className="fixed w-full top-0 z-50 bg-white text-foreground border-y-2">
+    <header className="w-full top-0 z-50 bg-white text-foreground border-y-2">
       {/* <div className="h-px w-full bg-neutral200 border-2" /> */}
 
       <div className="hidden md:block">
-        <div className="mx-auto flex h-14 max-w-[1200px] items-center justify-center">
+        <div className="mx-auto flex h-[84px] max-w-[1200px] items-center justify-center">
           <Link href="/" className="flex items-center gap-2">
             <Image
               src="/logo-default.svg"
               alt="Cannex Logo"
-              width={160}
-              height={32}
+              width={200}
+              height={40}
             />
           </Link>
         </div>
@@ -100,15 +100,6 @@ export default function NavBar({ state = "navbar" }: NavBarProps) {
         </NavBarBase>
       </div>
 
-      {/* <div
-        className={`fixed inset-0 z-40 bg-black/40 transition-opacity md:hidden ${
-          open
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none"
-        }`}
-        onClick={() => setOpen(false)}
-      /> */}
-
       {open && (
         <nav className="flex flex-col gap-0 p-4">
           {MENUS.map((m, i) => (
@@ -130,62 +121,6 @@ export default function NavBar({ state = "navbar" }: NavBarProps) {
           />
         </nav>
       )}
-      {/* TODO: uncomment */}
-      {/* {open && (
-        <aside
-          id={panelId}
-          className={`fixed right-0 top-0 z-50 h-full w-[78%] max-w-[340px] translate-x-full bg-white shadow-xl transition-transform  ${
-            open ? "translate-x-0" : ""
-          }`}
-          role="dialog"
-          aria-modal="true"
-        >
-          <div className="flex items-center justify-between border-b border-neutral200 px-4 py-3">
-            <Link
-              href="/"
-              onClick={() => setOpen(false)}
-              className="flex items-center gap-2"
-            >
-              <span
-                aria-hidden
-                className="inline-block h-5 w-5 rounded-sm"
-                style={{ background: "var(--primary500)" }}
-              />
-              <span className="text-lg font-semibold tracking-tight">
-                cannex
-              </span>
-            </Link>
-            <button
-              aria-label="Close menu"
-              onClick={() => setOpen(false)}
-              className="rounded-md p-2"
-            >
-              <Hamburger state="close" />
-            </button>
-          </div>
-          <nav className="flex flex-col gap-0 p-4">
-            {MENUS.map((m, i) => (
-              <div key={m.path} className="flex flex-col">
-                <Menu
-                  title={m.title}
-                  href={m.path}
-                  onClick={() => setOpen(false)}
-                  variant="mobile"
-                />
-                {i < MENUS.length - 1 && <div className="h-px bg-neutral200" />}
-              </div>
-            ))}
-            <Button
-              href="/contact"
-              fullWidth
-              className="mt-4"
-              onClick={() => setOpen(false)}
-            />
-          </nav>
-        </aside>
-      )} */}
-
-      {/* <div className="h-px w-full bg-neutral200" /> */}
     </header>
   );
 }
