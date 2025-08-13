00ae32cbe0ab86b72e10716d7606ba5c54ec5e3e
Thanaphorn Wutthikunprapan <twutthikunprapan@gmail.com>
2025-08-13 09:57:30 +0700

    feat: Add detail contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,17 +10,17 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="">
-      <div className="pb-20 relative overflow-visible">
+    <div className="w-full">
+      <div className="w-full pb-20 relative overflow-visible">
         <Image
           src="/contact-banner-img.jpg"
           alt="Contact Banner"
-          className=""
+          className="w-full object-cover"
           width={1920}
           height={400}
           quality={100}
         />
-        <div className="bg-white absolute top-[50vh] left-20 right-20">
+        <div className="bg-white absolute top-[50vh] left-20 right-20 border-b-[2px]">
           <div className="pl-16 pt-16 flex flex-col gap-2">
             <p className="text-7xl font-bold">Connect with Cannex</p>
             <p className="font-medium">
@@ -47,6 +47,46 @@ export default function Page() {
                 width="w-fit"
                 prefixIcon={<EmailIcon />}
               />
+            </div>
+          </div>
+          <div className="mt-8 flex px-16 w-full ">
+            <div className="w-full border-[2px_2px_0px_2px]">
+              <div className="w-full h-[324px]">
+                <Image
+                  src="/headquarter-img.png"
+                  alt="headquarter"
+                  className="w-full h-full object-cover"
+                  width={1920}
+                  height={400}
+                  quality={100}
+                />
+              </div>
+              <div className="py-8 px-6 border-t-[2px] relative">
+                32/27 Sino-Thai Tower, 6F, Sukhumvit 21 Rd. (Asok),
+                Klongtoey-Nua, Wattana, Bangkok 10110, Thailand
+                <p className="text-[20px] text-white font-bold bg-black py-2.5 px-2.5 absolute top-0 left-6 transform -translate-y-1/2">
+                  Headquarter
+                </p>
+              </div>
+            </div>
+            <div className="w-full border-[2px_2px_0px_0px]">
+              <div className="w-full h-[324px]">
+                <Image
+                  src="/lab-img.png"
+                  alt="lab"
+                  className="w-full h-full object-cover"
+                  width={1920}
+                  height={400}
+                  quality={100}
+                />
+              </div>
+              <div className="py-8 px-6 border-t-[2px] relative">
+                B1.2, 408/7, Moo.7, Sukhumvit Rd., Bangpoo Mai, Mueang Samut
+                Prakan, Samut Prakan 10280, Thailand
+                <p className="text-[20px] text-white font-bold bg-black py-2.5 px-2.5 absolute top-0 left-6 transform -translate-y-1/2">
+                  Lab
+                </p>
+              </div>
             </div>
           </div>
         </div>
