8c3bfa39b00b49514fed3652c736e0bb373a8c9d
algnot <thanawat.tongla@gmail.com>
2025-08-13 12:37:20 +0700

    feat: enhance newsroom page layout with date formatting and improved styling

--- src/app/newsroom/[slugId]/page.tsx
+++ src/app/newsroom/[slugId]/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { BackendClient } from "@/lib/backend-client";
+import { formatDate } from "@/utils/format";
 import React from "react";
 
 type Params = Promise<{ slugId: string }>;
@@ -18,9 +19,19 @@ export default async function Page({ params }: { params: Params }) {
 
   return (
     <div>
-      <div className="flex">
-        <div className=""></div>
-        <img src={data.image.url} alt={data.image.name} />
+      <div className="flex h-[475px]">
+        <div className="w-[56%] pt-[96px] pl-[80px]">
+          <div className="w-fit text-white bg-black p-[2px_8px]">Newsroom</div>
+          <div className="text-[52px] w-[83%] font-bold line-clamp-2">
+            {data.title}
+          </div>
+          <div className="text-gray-400">{formatDate(data.createdAt)}</div>
+        </div>
+        <img
+          className="w-[44%] border-l-2 object-cover"
+          src={data.image.url}
+          alt={data.image.name}
+        />
       </div>
     </div>
   );
