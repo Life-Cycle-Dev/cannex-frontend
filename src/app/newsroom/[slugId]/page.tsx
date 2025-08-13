b86b952ae64f0535c0ade0a9ed0f39a940498acf
Thanaphorn Wutthikunprapan <twutthikunprapan@gmail.com>
2025-08-13 19:28:47 +0700

    feat: add ShareIcon component and integrate it into the newsroom page layout

--- src/app/newsroom/[slugId]/page.tsx
+++ src/app/newsroom/[slugId]/page.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+import Button from "@/components/Button";
+import ShareIcon from "@/components/icons/ShareIcon";
 import { BackendClient } from "@/lib/backend-client";
 import { formatDate } from "@/utils/format";
 import { notFound } from "next/navigation";
@@ -19,20 +21,41 @@ export default async function Page({ params }: { params: Params }) {
   const data = response.data[0];
 
   return (
-    <div>
-      <div className="flex h-[475px]">
-        <div className="w-[56%] pt-[96px] pl-[80px]">
-          <div className="w-fit text-white bg-black p-[2px_8px]">Newsroom</div>
-          <div className="text-[52px] w-[83%] font-bold line-clamp-2">
-            {data.title}
+    <div className="w-full">
+      <div className="flex flex-col tablet:flex-row tablet:border-b-[2px]">
+        <div className="p-[32px_20px_40px_20px] tablet:p-[96px_64px_40px_80px] w-full flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
+            <div className="w-fit text-white bg-black p-[2px_8px]">
+              Newsroom
+            </div>
+            <div className="text-[46px] font-bold break-words leading-[110%]">
+              {data.title}
+            </div>
           </div>
-          <div className="text-gray-400">{formatDate(data.createdAt)}</div>
+          <div className="w-full h-full flex tablet:flex-col items-center tablet:items-start justify-between">
+            <div className="text-gray-400">{formatDate(data.createdAt)}</div>
+            <Button
+              text="Share"
+              type="secondaryBlack"
+              width="w-fit"
+              suffixIcon={<ShareIcon />}
+            />
+          </div>
         </div>
-        <img
-          className="w-[44%] border-l-2 object-cover"
-          src={data.image.url}
-          alt={data.image.name}
-        />
+
+        <div className="w-full h-[280px] tablet:h-[475px] border-[2px_0px_2px_0px] tablet:border-[0px_0px_0px_2px]">
+          <img
+            className="w-full h-full object-cover"
+            src={data.image.url}
+            alt={data.image.name}
+          />
+        </div>
+      </div>
+      <div className="w-full p-[48px_20px_137px_20px] tablet:p-[64px_0px_128px_0px] tablet:mx-auto tablet:max-w-[842px]">
+        testetste Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+        officia beatae aperiam non quia. Exercitationem deserunt eligendi, quo
+        ullam temporibus, reiciendis voluptate quibusdam molestias assumenda
+        aliquam corporis libero. Nesciunt, minus!
       </div>
     </div>
   );
---
+++ src/components/icons/ShareIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ShareIcon({ className = "" }: { className?: string }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g clipPath="url(#clip0_824_7329)">
+        <path d="M7.5 3H3V21H21V16.125" stroke="black" strokeWidth="1.875" />
+        <path
+          d="M19.96 5.44843L11.368 14.0404L9.95651 12.6289L18.5475 4.0369L10.9757 4.0369V2.04039L21.9565 2.04039L21.9565 13.0212L19.96 13.0212L19.96 5.44843Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_824_7329">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
