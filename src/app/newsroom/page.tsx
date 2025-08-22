7d71d7c06cd26cdaeedc0a91d3bed72d25b78d20
Thanaphorn Wutthikunprapan <twutthikunprapan@gmail.com>
2025-08-22 21:42:48 +0700

    fix issue 7

--- src/app/newsroom/page.tsx
+++ src/app/newsroom/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
       <div className="px-[20px] tablet:px-0">
         <div className="flex justify-between">
           <div className="min-w-[79px] h-[176px] border-r-2 hidden tablet:block"></div>
-          <h1 className="w-full border-l-2 border-r-2 tablet:border-r-0 tablet:border-l-0 font-bold text-[46px] p-[32px_0px_32px_32px] tablet:text-7xl tablet:p-[64px_0px_32px_40px]">
+          <h1 className="w-full border-l-2 border-r-2 tablet:border-r-0 tablet:border-l-0 font-bold text-[46px] p-[32px_16px_32px_16px] tablet:text-7xl tablet:p-[64px_0px_32px_40px]">
             Newsroom
           </h1>
           <div className="min-w-[79px] h-[176px] border-l-2 hidden tablet: tablet:block"></div>
@@ -23,7 +23,7 @@ export default async function Page() {
 
       <div className="border-t-2" />
       <div className="px-[20px] tablet:px-0 min-h-[calc(100vh-200px)] max-h-[calc(100vh-200px)] tablet:min-h-[calc(100vh-250px)] tablet:max-h-[calc(100vh-250px)] desktop:min-h-[calc(100vh-311px)] desktop:max-h-[calc(100vh-311px)]">
-        <div className="h-full mb-[64px] tablet:mb-0">
+        <div className="mb-[64px] tablet:mb-0">
           <NewsroomCarousel items={items} />
         </div>
       </div>
