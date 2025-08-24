269b1c5034f49901672eef87ff2f8200dad87e9c
ttonkla <tonkla.thanawat@q-chang.com>
2025-08-24 12:50:02 +0700

    Update Privacy Policy and Terms of Use pages to use headings for improved accessibility and styling consistency.

--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  return <h1 className="text-[72px] p-[20px] tablet:p-[80px_299px] font-bold">Terms of Use</h1>;
 }
